db6dd8aa658f0109159bc8174edd7b26d880aa3b
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-01-14 15:27:30 -0400

    métodos, tipos de paramentros

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.json())
+//Métodos
 // GET, buscando uma informação;
 // POST, criar uma informação;
 // PUT, editar uma recurso na aplicação;
 // DELETE, remover informação;
+
+//Tipos de parametros
+// Query Params: request.query  (filtros, ordenação, paginação,...)
+// Route Params: request.params - PUT, DELETE (Identificar um recurso na alteeração ou remoção)
+// Body: POST request.body (Dados para criação ou alteração de um registro)
+
 
 app.get('/', (request, response) => {
   return response.json({message:'Hello World'});
