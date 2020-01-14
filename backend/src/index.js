43dddc4dc9783391e0ac1534255d91b23a9d7456
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-01-14 15:59:48 -0400

    organizando em arquivos, rotas, models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://evertonstm:evellyn1@oministack-7o1jy.mongodb.net/week10?retryWrites=true&w=majority',{
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 //Métodos
 // GET, buscando uma informação;
 // POST, criar uma informação;
@@ -18,10 +20,5 @@ app.use(express.json())
 // Query Params: request.query  (filtros, ordenação, paginação,...)
 // Route Params: request.params - PUT, DELETE (Identificar um recurso na alteeração ou remoção)
 // Body: POST request.body (Dados para criação ou alteração de um registro)
-
-
-app.get('/', (request, response) => {
-  return response.json({message:'Hello World'});
-});
 
 app.listen(3333);
