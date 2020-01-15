2d82a40aa81723fb35dbbfe6fdd83c7f7aff227a
Everton Reis <evertonreis@Mac-mini-de-Everton.local>
2020-01-15 15:24:14 -0400

    Estruturação do projeto criado

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/* 
+  Componente: Bloco isolado de HTML, CSS, JS, o qual não interfere no restante da aplicação;
+  Propriedade: Informações que um componente PAI passa para o componente FILHO.
+  Estado; Informações mantidas pelo componente (Lembrar: Imutabilidade)
+    */
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
