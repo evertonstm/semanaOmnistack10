d278e577748a74337efe49ada62a5aa13b7106f4
evertonstm <evertonstm@gmail.com>
2020-01-22 16:22:12 -0400

    add navigation

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -33,13 +33,58 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{latitude: 2.8287535, longitude:-60.6636229 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/22085525?v=4' }} />
+
+        <Callout onPress={() =>{
+          navigation.navigate('Profile', { github_username: 'evertonstm' });
+        }}>
+          <View style={styles.Callout}>
+            <Text style={styles.devName}>Everton dos Reis</Text>
+            <Text style={styles.devBio}>evertonstm</Text>
+            <Text style={styles.devTechs}>ReactJs, React Native, Node.js</Text>
+          </View>
+        </Callout>
+
+      </Marker>
+    </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1    
   },
+
+  avatar:{
+    width:54,
+    height:54,
+    borderRadius: 4,
+    borderWidth:4, 
+    borderColor: '#fff'
+  },
+
+  Callout: {
+    width: 260,
+  },
+
+  devName:{
+    fontWeight:'bold',
+    fontSize:16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs:{
+    marginTop:5,
+  },
+
+
 })
 
 export default Main;
