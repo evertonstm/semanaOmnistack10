ecf8740db89c720f5bc3064396f2f04251234e2a
evertonstm <evertonstm@gmail.com>
2020-01-24 13:53:23 -0400

    url api axios

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL:'exp://at-35e.anonymous.mobile.exp.direct:80',
-  baseURL:'http://10.0.4.121:3333',
+  baseURL:'exp://at-35e.anonymous.mobile.exp.direct:80',
+  //baseURL:'http://10.0.4.121:3333',
 });
 
 export default api;
